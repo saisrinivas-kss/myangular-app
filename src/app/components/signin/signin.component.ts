871b87b4d7bbdf689c7582e1010ae541a4e4e814
saisrinivas <saisrinivas.k009@gmail.com>
2020-08-21 09:30:00 +0530

    sidenavupdate

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
+
+  public user = ()=>{
+    
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/the24spokes.component.ts
+++ src/app/components/the24spokes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit,  } from '@angular/core';
-import { CovidindiaService } from '../services/covidindia.service'
-import { HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'the24spokesNews',
