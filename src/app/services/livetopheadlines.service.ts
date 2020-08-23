63459e338e44f13c0ffa5c2c76bc0b03ac45fc40
saisrinivas <saisrinivas.k009@gmail.com>
2020-08-23 11:59:42 +0530

    https

--- src/app/services/livetopheadlines.service.ts
+++ src/app/services/livetopheadlines.service.ts
@@ -10,6 +10,6 @@ export class LivetopheadlinesService {
   constructor(public topheadlines:HttpClient) { }
 
   public topheadlinesnewinindia():Observable<any>{
-    return this.topheadlines.get("http://newsapi.org/v2/top-headlines?country=in&apiKey=8da0d5cf661e44599f9d53fd4009b7c8");
+    return this.topheadlines.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=8da0d5cf661e44599f9d53fd4009b7c8");
   }
 }
